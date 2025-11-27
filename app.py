322d509f467c1e450693a6ca742c5334836a1c86
kozk <kozkunadm@nube.undmexico.mx>
2025-11-27 11:13:24 -0600

    20251127 00

--- app.py
+++ app.py
@@ -7,19 +7,28 @@ cliente = MongoClient("mongodb+srv://itzcui_db_user:8Zu4Fik8n6OmwHCw@cdmx.k0skwr
 app.db = cliente.blog
 
 entradas = [entrada for entrada in app.db.contenido.find({})]
+usuarios = [usuario for usuario in app.db.usuarios.find({})]
 print(entradas)
+print(usuarios)
 
 @app.route("/", methods = ["GET", "POST"])
 def home():
     if request.method == "POST":
+        fecha_formato = datetime.datetime.today().strftime("%d-%m-%Y, %H:%M:%S")
         titulo = request.form.get("tit")
         contenido_entrante = request.form.get("content")
-        fecha_formato = datetime.datetime.today().strftime("%d -%m-%Y")
         parametros = {"titulo": titulo, "contenido": contenido_entrante, "fecha": fecha_formato}
         entradas.append(parametros)
         app.db.contenido.insert_one(parametros)
 
+        nombre_usuario = request.form.get("name_u")
+        telefono = request.form.get("phone_u")
+        parametros_usuario = {"nombre_u": nombre_usuario, "telefono_u": telefono, "fecha": fecha_formato}
+        usuarios.append(parametros_usuario)
+        app.db.usuarios.insert_one(parametros_usuario)
+
     return render_template("index.html", entradas = entradas)
+
 
 if __name__ == "__main__":
     app.run()
